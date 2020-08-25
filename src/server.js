f60f4a6471953e0ab194b65d2dc8008cb2d526b2
Banzak <l.santana123@hotmail.com>
2020-08-24 21:49:22 -0300

    refatorando server.js

--- src/server.js
+++ src/server.js
@@ -1,21 +1,52 @@
+const proffys = [
+    { name: "Diego Rodrigues", 
+    avatar:"https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4", 
+    whatsapp: "75981460783", 
+    bio: "Entusiasta das melhores tecnologias de química avançada.", 
+    subject: "Química", 
+    cost: "20", 
+    weekday: [0], 
+    time_from: [720], 
+    time_to: [1240] 
+    },
+
+    { name: "Leonardo Santana", 
+    avatar:"https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4", 
+    whatsapp: "75981460783", 
+    bio: "Entusiasta das melhores tecnologias de matemática avançada.", 
+    subject: "Matematica", 
+    cost: "20", 
+    weekday: [2], 
+    time_from: [720], 
+    time_to: [1240] 
+    }
+]
+
+function pageLanding(req, res) {
+    return res.sendFile(__dirname + "/views/index.html")
+}
+
+function pageStudy(req, res) {
+    return res.sendFile(__dirname + "/views/study.html")
+}
+
+function pageGiveClasses(req, res) {
+    return res.sendFile(__dirname + "/views/give-classes.html")
+}
+
 const express = require('express')
 const server = express()
+const nunjucks = require('nunjucks')
 
-server.use(express.static("public"))
-
-.get("/", (req, res) => {
-    return res.sendfile(__dirname + "/views/index.html")
+nunjucks.configure('src/views', {
+    express: server,
+    noCache: true,
 })
 
-.get("/study", (req, res) => {
-    return res.sendfile(__dirname + "/views/study.html")
-})
-
-.get("/give-classes", (req, res) => {
-    return res.sendfile(__dirname + "/views/give-classes.html")
-})
-
-
-
+server
+.use(express.static("public"))
+.get("/", pageLanding)
+.get("/study", pageStudy)
+.get("/give-classes", pageGiveClasses)
 .listen(5500)
 
